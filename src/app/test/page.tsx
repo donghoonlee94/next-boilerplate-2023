2c9be530c223a23233c7832e997694f00a05a892
donghoonlee94 <94donghoon.lee@gmail.com>
2023-08-02 20:13:07 +0900

    feature: default axios instance

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import React from 'react';
 import axios from 'axios';
 import Input from '@/app/test/components/input';
 
 const Test: React.FC = () => {
-  axios.get('http://localhost:3000/api/hello').then(response => console.log('API Test', response.data));
+  axios.get('/api/hello').then(response => console.log('API Test', response.data));
 
   return (
     <div>
